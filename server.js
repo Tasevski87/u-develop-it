4f69d90e4e758f0c4676a872346a1066767451ac
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-01-18 11:37:33 -0500

    connecting to express

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,23 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'toshetasevski',
+        // Your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 
